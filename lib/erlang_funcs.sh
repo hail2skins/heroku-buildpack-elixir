930c7d3dc89dc3c7a8e24d7dba522889ce1d25be
Art Mills <art@hamcois.com>
2017-07-28 21:56:01 +0000

    Reset to initial

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -7,21 +7,21 @@ function download_erlang() {
   erlang_package_url="${erlang_package_url}/$(erlang_tarball)"
 
   # If a previous download does not exist, then always re-download
-  if [ ! -f $(erlang_tarball) ]; then
+  if [ ! -f ${cache_path}/$(erlang_tarball) ]; then
     clean_erlang_downloads
 
     # Set this so elixir will be force-rebuilt
     erlang_changed=true
 
     output_section "Fetching Erlang ${erlang_version}"
-    curl -s ${erlang_package_url} -o ${erlang_tarball} || exit 1
+    curl -s ${erlang_package_url} -o ${cache_path}/$(erlang_tarball) || exit 1
   else
     output_section "Using cached Erlang ${erlang_version}"
   fi
 }
 
 function clean_erlang_downloads() {
-  rm -rf OTP-*.tar.gz
+  rm -rf ${cache_path}/OTP-*.tar.gz
 }
 
 function install_erlang() {
