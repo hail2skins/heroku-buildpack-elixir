a12f3f8cc7a31941f003fd8e5f8327a441d78b48
Art Mills <art@hamcois.com>
2017-07-28 21:46:11 +0000

    Change back complile, but make tweak to erlang_func download.

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -7,14 +7,14 @@ function download_erlang() {
   erlang_package_url="${erlang_package_url}/$(erlang_tarball)"
 
   # If a previous download does not exist, then always re-download
-  if [ ! -f ${cache_path}/$(erlang_tarball) ]; then
+  if [ ! -f $(erlang_tarball) ]; then
     clean_erlang_downloads
 
     # Set this so elixir will be force-rebuilt
     erlang_changed=true
 
     output_section "Fetching Erlang ${erlang_version}"
-    curl -s ${erlang_package_url} -o ${cache_path}/$(erlang_tarball) || exit 1
+    curl -s ${erlang_package_url} -o $(erlang_tarball) || exit 1
   else
     output_section "Using cached Erlang ${erlang_version}"
   fi
