b1658c4ac50da232288dcdf03ded41ac84cae3f3
Art Mills <art@hamcois.com>
2017-07-28 21:51:09 +0000

    Another try

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -14,7 +14,7 @@ function download_erlang() {
     erlang_changed=true
 
     output_section "Fetching Erlang ${erlang_version}"
-    curl -s ${erlang_package_url} -o OTP-19.3.tar.gz || exit 1
+    curl -s ${erlang_package_url} || exit 1
   else
     output_section "Using cached Erlang ${erlang_version}"
   fi
