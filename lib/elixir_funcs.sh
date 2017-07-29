1ebe3912e7b537a0124872b9665336accff16a5b
Art Mills <art@hamcois.com>
2017-07-29 04:44:41 +0000

    Apparently path redirects.  Fuck you path.

--- lib/elixir_funcs.sh
+++ lib/elixir_funcs.sh
@@ -10,7 +10,7 @@ function download_elixir() {
     output_section "Fetching Elixir ${elixir_version} for OTP ${otp_version}"
 
     local download_url="https://github.com/elixir-lang/elixir/releases/download/${elixir_version}/Precompiled.zip"
-    curl -s "https://github.com/elixir-lang/elixir/archive/v1.4.4.zip" -o ${cache_path}/$(elixir_download_file)
+    curl -s "https://codeload.github.com/elixir-lang/elixir/zip/v1.4.4" -o ${cache_path}/$(elixir_download_file)
     
     if [ $? -ne 0 ]; then
       output_section "Falling back to fetching Elixir ${elixir_version} for generic OTP version"
