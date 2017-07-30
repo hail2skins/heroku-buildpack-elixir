c1f8869d5d8c36cd459270be970b83b79299dab4
Art Mills <art@hamcois.com>
2017-07-30 04:24:00 +0000

    Change cache path with brackets

--- lib/elixir_funcs.sh
+++ lib/elixir_funcs.sh
@@ -31,7 +31,7 @@ function install_elixir() {
 
   cd - > /dev/null
 
-  chmod +x $(cache_path)/elixir-v1.4.4/bin/*
+  chmod +x "${cache_path}/elixir-v1.4.4/bin/*"
   PATH=$(elixir_path)/bin:${PATH}
 
   export LC_CTYPE=en_US.utf8
